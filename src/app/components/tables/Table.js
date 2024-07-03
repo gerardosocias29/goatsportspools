1766415a8962554f7fc1e904e1e7954d76c04850
Gerardo <g.socias29@gmail.com>
2024-07-04 01:20:05 +0800

    partial

---
+++ src/app/components/tables/Table.js
@@ -0,0 +1,123 @@
+import { Button } from "primereact/button";
+import { Column } from "primereact/column";
+import { DataTable } from "primereact/datatable";
+import { useEffect, useState } from "react";
+import { InputText } from "primereact/inputtext";
+import { Dropdown } from "primereact/dropdown";
+import { Sidebar } from "primereact/sidebar";
+import { FilterMatchMode } from "primereact/api";
+
+const Table = ({ 
+  checkbox = false, columns, data,
+    actions = false, action_types = {
+      view: true,
+      edit: false,
+      delete: false,
+    }, customActions = null, customActionsWidth = '3rem', 
+    expandableRow = false, onRowExpand, onRowCollapse, rowExpansionTemplate,
+    paginator = true,
+    actionsClicked,
+    hasOptions = false,
+    scrollable = false,
+    scrollHeight = "400px"
+  }) => {
+ 
+  const [selectedData, setSelectedData] = useState([]);
+  const [expandedRows, setExpandedRows] = useState(null);
+
+  const renderActions = (data) => {
+    // console.log(data)
+    return (
+      <div className="flex justify-center">
+        { action_types.view && <Button className="text-purple ring-0" icon="pi pi-eye" rounded text aria-label="View" onClick={(e) => actionsClicked && actionsClicked(data.id, 'view', data) }/> }
+        { action_types.edit && <Button className="text-blue-500 ring-0" icon="pi pi-pencil" rounded text aria-label="Edit" onClick={(e) => actionsClicked && actionsClicked(data.id, 'edit', data) }/> }
+        { action_types.delete && <Button className="text-red-500 ring-0" icon="pi pi-trash" rounded text aria-label="Trash" onClick={(e) => actionsClicked && actionsClicked(data.id, 'trash', data) }/> }
+      </div>
+    );
+  }
+
+  const renderTemplate = (value, template, rowData, field) => { return ( template(value, rowData, field) ) }
+
+  const itemsPerPage = [
+    { value: '10', name: '10'},
+    { value: '50', name: '50'},
+    { value: '100', name: '100'},
+  ]
+  const [pageItem, setPageItem] = useState(itemsPerPage[0]);
+  const [filterVisible, setFilterVisible] = useState(false);
+
+  const [globalFilterValue, setGlobalFilterValue] = useState('');
+  const [filters, setFilters] = useState({
+    global: { value: null, matchMode: FilterMatchMode.CONTAINS },
+  });
+
+  const onGlobalFilterChange = (e) => {
+    const value = e.target.value;
+    let _filters = { ...filters };
+    _filters['global'].value = value;
+    setFilters(_filters);
+    setGlobalFilterValue(value);
+  };
+
+
+  useEffect(() => {
+    // fetch api then set the data
+  }, []);
+
+  return (
+  <>
+    {
+      hasOptions &&
+      <div className="mb-4 flex flex-col md:flex-row gap-2 justify-between items-center">
+        <div className="rounded-lg border border-gray w-full md:w-1/3 flex items-center">
+          <i className="pi pi-search p-2 text-label"></i>
+          <InputText value={globalFilterValue} onChange={onGlobalFilterChange} placeholder="Search items..." className="border-none bg-transparent ring-0 w-full" />
+        </div>
+        <div className="flex w-full md:auto justify-end gap-2 flex-wrap">
+          <div className="flex justify-center gap-2">
+            <div className="flex items-center gap-2">
+              <label className="text-label">Items per page</label>
+              <div className="rounded-lg border border-gray">
+                <Dropdown value={pageItem.value} onChange={(e) => {setPageItem(e)}} options={itemsPerPage} optionLabel="name" className="border-none ring-0 w-full bg-transparent" />
+              </div>
+            </div>
+          </div>
+          <div>
+            <Button className="bg-transparent text-black rounded-lg border-gray w-full md:auto" iconPos="right" label="Filter" icon="pi pi-filter" onClick={() => setFilterVisible(true)} />
+            <Sidebar visible={filterVisible} position="right" onHide={() => setFilterVisible(false)}>
+              <h2>Filter Here</h2>
+            </Sidebar>
+          </div>
+        </div>
+      </div>
+    }
+    <DataTable value={data} paginator={paginator} rows={(paginator ? pageItem.value : undefined)} filters={filters}
+      emptyMessage={(<>
+        <div className="flex flex-col items-center gap-4 text-center justify-center">
+          <p className="text-3xl">No Data Found</p>
+        </div>
+      </>)}
+      selectionMode={!checkbox ? null : 'checkbox'} rowHover={true} selection={selectedData} onSelectionChange={(e) => setSelectedData(e.value)} dataKey="id"
+      onRowToggle={(e) => setExpandedRows(e.data)} expandedRows={expandedRows} onRowExpand={onRowExpand} onRowCollapse={onRowCollapse} rowExpansionTemplate={rowExpansionTemplate}
+      scrollable={scrollable} scrollHeight={scrollHeight}
+    >
+      {checkbox && <Column headerClassName="bg-background text-white rounded-tl-xl rounded-bl-xl border-none" selectionMode="multiple" headerStyle={{ width: '3rem' }} />}
+        {
+          columns.map((col, i) => {
+            return <Column 
+              key={i} 
+              headerClassName={`${col.headerClassName? col.headerClassName:''} text-center bg-background text-white ${(i==0 && !checkbox ? 'rounded-tl-xl rounded-bl-xl' : '' )} ${(i === (columns.length-1) && !actions)? 'rounded-tr-xl rounded-br-xl' : ''} ${ expandableRow && col.has_expander ? '' : 'border-white border'} font-normal text-white`}
+              field={col.field} 
+              header={col.header}
+              body={(data) => col.hasTemplate ? renderTemplate(data[col.field], col.template, data, col.field) : data[col.field]}
+            />;
+          })
+        }
+        {actions && !customActions && <Column headerClassName="bg-background text-white rounded-tr-xl rounded-br-xl font-normal text-white border-t" header={'Actions'} headerStyle={{ width: '3rem' }} body={renderActions}/>}
+        {actions && customActions && <Column headerClassName="bg-background text-white rounded-tr-xl rounded-br-xl font-normal text-white border-t" header={'Actions'} headerStyle={{ width: customActionsWidth }} body={customActions}/>}
+    </DataTable>
+  </>
+  );
+}
+
+export default Table;
--- src/app/pages/screens/games/NFL.js
+++ src/app/pages/screens/games/NFL.js
@@ -1,10 +1,17 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import LazyTable from "../../../components/tables/LazyTable";
-import { DateTemplate, TeamTemplate } from "./NFLTemplates";
+import { BetDateTemplate, TeamTemplate } from "./NFLTemplates";
 import { Button } from "primereact/button";
 import { Tooltip } from "primereact/tooltip";
+import { Dropdown } from "primereact/dropdown";
+import { useAxios } from "../../../contexts/AxiosContext";
+import { Dialog } from "primereact/dialog";
+import Table from "../../../components/tables/Table";
+import moment from "moment";
 
 const NFL = () => {
+  const axiosService = useAxios();
+
   const [refreshTable, setRefreshTable] = useState(false);
   const [bets, setBets] = useState([]);
   const wagerTypes = [
@@ -14,6 +21,35 @@ const NFL = () => {
   ];
 
   const [activeWagerType, setActiveWagerType] = useState(wagerTypes[0]);
+  const BetTypeTemplate = () => {
+    return <div className="text-center">{activeWagerType.name}</div>
+  }
+  const WagerTypeTemplate = (data) => {
+    let type = data;
+    switch(data) {
+      case 'total': type = 'Total Points'; break;
+      case 'moneyline': type = 'Money Line'; break;
+      case 'spread': type = 'Spread'; break;
+    }
+    return <div className="text-center">{type}</div>
+  }
+  const BetDateTemplate = (value, game) => {
+    return <div className="text-center">{moment(game.data?.game_datetime).format('MMM DD')}</div>
+  }
+  const betsColumn = [
+    { field: 'type', header: 'Bet Type', headerClassName: 'w-[120px]', template: BetTypeTemplate, hasTemplate: true},
+    { field: 'type', header: 'Wager Type', headerClassName: 'w-[150px]', template: WagerTypeTemplate, hasTemplate: true},
+    { field: 'type', header: 'Game Date', headerClassName: 'w-[150px]', template: BetDateTemplate, hasTemplate: true},
+    { field: '', header: 'Team'},
+    { field: '', header: 'Bet Amount'},
+  ]
+
+  const handleBetActionsClick = (value, type, betData) =>{
+    console.log(betData, type);
+    if(type == "trash"){
+      handleBetClick(betData);
+    }
+  }
 
   const handleBetClick = (bet) => {
     setBets((prevBets) => {
@@ -43,11 +79,11 @@ const NFL = () => {
       <div className="flex flex-col gap-4">
         <Button label={odd.favored_points}
           className={`border-primaryS ring-0 text-primary hover:text-white hover:bg-primaryS w-full bg-transparent rounded-lg ${isBetSelected({ type: 'spread', team: odd.favored_team, points: odd.favored_points }) ? 'bg-primaryS text-white' : ''}`}
-          onClick={() => handleBetClick({ type: 'spread', team: odd.favored_team, points: odd.favored_points })}
+          onClick={() => handleBetClick({ type: 'spread', team: odd.favored_team, points: odd.favored_points, bet_amount: 0, data: data })}
         />
         <Button label={odd.underdog_points}
           className={`border-primaryS ring-0 text-primary hover:text-white hover:bg-primaryS w-full bg-transparent rounded-lg ${isBetSelected({ type: 'spread', team: odd.underdog_team, points: odd.underdog_points }) ? 'bg-primaryS text-white' : ''}`}
-          onClick={() => handleBetClick({ type: 'spread', team: odd.underdog_team, points: odd.underdog_points })}
+          onClick={() => handleBetClick({ type: 'spread', team: odd.underdog_team, points: odd.underdog_points, bet_amount: 0, data: data })}
         />
       </div>
     );
@@ -60,11 +96,11 @@ const NFL = () => {
       <div className="flex flex-col gap-4">
         <Button label={'o' + odd.over_total}
           className={`border-primaryS ring-0 text-primary hover:text-white hover:bg-primaryS w-full bg-transparent rounded-lg ${isBetSelected({ type: 'total', team: 'over', points: odd.over_total }) ? 'bg-primaryS text-white' : ''}`}
-          onClick={() => handleBetClick({ type: 'total', team: 'over', points: odd.over_total })}
+          onClick={() => handleBetClick({ type: 'total', team: 'over', points: odd.over_total, bet_amount: 0, data: data })}
         />
         <Button label={'u' + odd.under_total}
           className={`border-primaryS ring-0 text-primary hover:text-white hover:bg-primaryS w-full bg-transparent rounded-lg ${isBetSelected({ type: 'total', team: 'under', points: odd.under_total }) ? 'bg-primaryS text-white' : ''}`}
-          onClick={() => handleBetClick({ type: 'total', team: 'under', points: odd.under_total })}
+          onClick={() => handleBetClick({ type: 'total', team: 'under', points: odd.under_total, bet_amount: 0, data: data })}
         />
       </div>
     );
@@ -77,27 +113,60 @@ const NFL = () => {
       <div className="flex flex-col gap-4">
         <Button label={odd.favored_ml}
           className={`border-primaryS ring-0 text-primary hover:text-white hover:bg-primaryS w-full bg-transparent rounded-lg ${isBetSelected({ type: 'moneyline', team: odd.favored_team, ml: odd.favored_ml }) ? 'bg-primaryS text-white' : ''}`}
-          onClick={() => handleBetClick({ type: 'moneyline', team: odd.favored_team, ml: odd.favored_ml })}
+          onClick={() => handleBetClick({ type: 'moneyline', team: odd.favored_team, ml: odd.favored_ml, bet_amount: 0, data: data })}
         />
         <Button label={odd.underdog_ml}
           className={`border-primaryS ring-0 text-primary hover:text-white hover:bg-primaryS w-full bg-transparent rounded-lg ${isBetSelected({ type: 'moneyline', team: odd.underdog_team, ml: odd.underdog_ml }) ? 'bg-primaryS text-white' : ''}`}
-          onClick={() => handleBetClick({ type: 'moneyline', team: odd.underdog_team, ml: odd.underdog_ml })}
+          onClick={() => handleBetClick({ type: 'moneyline', team: odd.underdog_team, ml: odd.underdog_ml, bet_amount: 0, data: data })}
         />
       </div>
     );
   }
 
   const gamesColumn = [
-    { field: 'game_datetime', header: 'Date', template: DateTemplate, hasTemplate: true, headerClassName: 'w-[120px]' },
+    { field: 'game_datetime', header: 'Date', template: BetDateTemplate, hasTemplate: true, headerClassName: 'w-[120px]' },
     { field: 'team', header: 'Team', template: TeamTemplate, hasTemplate: true },
     { field: 'spread', header: 'Spread', headerClassName: 'w-[290px]', template: SpreadTemplate, hasTemplate: true },
     { field: 'tp', header: 'Total Points', headerClassName: 'w-[290px]', template: TotalTemplate, hasTemplate: true },
     { field: 'ml', header: 'Money Line', headerClassName: 'w-[290px]', template: MoneyLineTemplate, hasTemplate: true },
   ];
 
+  const [selectedLeague, setSelectedLeague] = useState();
+  const [joinedLeagues, setJoinedLeagues] = useState();
+  const getJoinedLeagues = () => {
+    axiosService.get('/api/leagues/joined').then((response) => {
+      if(response.data.status){
+        setJoinedLeagues(response.data.leagues_joined);
+        setSelectedLeague(response.data.leagues_joined[0] || []);
+      }
+    }).catch((error) => {
+      console.log(error);
+    });
+  }
+
+  const [modalWagerVisible, setModalWagerVisisble] = useState(false);
+
+
+  useEffect(() => {
+    getJoinedLeagues();
+  }, []);
+
   return (
-    <div className="flex flex-col gap-5 p-5">
-      <div className="flex items-center gap-2 text-primary text-3xl font-semibold">NFL</div>
+    <div className="flex flex-col gap-5 p-5" id="NFL">
+      <div className="flex items-center gap-2 justify-between">
+        <div className="text-primary text-3xl font-semibold">NFL</div>
+        <div className="flex items-center gap-4">
+          <p className="font-bold text-primary">League: </p>
+          <Dropdown 
+            placeholder="Select League"
+            className="rounded-lg w-[300px]"
+            value={selectedLeague}
+            onChange={(e) => setSelectedLeague(e.value)}
+            options={joinedLeagues}
+            optionLabel="name"
+          />
+        </div>
+      </div>
 
       <div className="w-full p-5 bg-white rounded-lg flex flex-col gap-1">
         <div>
@@ -106,7 +175,10 @@ const NFL = () => {
             {wagerTypes.map((wt, i) => (
               <div key={i}
                 className={`cursor-pointer select-none rounded-lg shadow-lg border p-4 text-center hover:bg-primaryS hover:text-white ${activeWagerType.value === wt.value ? 'bg-primaryS text-white' : 'bg-white'}`}
-                onClick={() => setActiveWagerType(wt)}
+                onClick={() => {
+                  setActiveWagerType(wt); 
+                  setBets([]);
+                }}
               >
                 <h2 className="text-xl font-semibold">{wt.name}</h2>
               </div>
@@ -128,7 +200,7 @@ const NFL = () => {
           />
         </div>
         <div className="flex items-center justify-end ">
-          <Button label="Continue" className="mt-5 rounded-lg bg-background text-white border-background ring-0 w-[200px]"/>
+          <Button label="Continue" className="mt-5 rounded-lg bg-background text-white border-background ring-0 w-[200px]" onClick={() => setModalWagerVisisble(true)}/>
         </div>
 
         {/* <div className="">
@@ -145,6 +217,16 @@ const NFL = () => {
           <Button label="Place Bet" className="w-full mt-5 bg-primaryS text-white" onClick={() => alert('Bet placed!')} />
         </div> */}
       </div>
+      <Dialog header="Your Bets" visible={modalWagerVisible} draggable={false} maximizable={false} className="w-2/3" onHide={() => setModalWagerVisisble(false)}>
+        <Table data={bets} 
+          columns={betsColumn} 
+          actions={true} action_types={{delete: true}} actionsClicked={handleBetActionsClick}
+          paginator={false}
+          scrollable={true} scrollHeight="450px"
+        />
+        <Button label="Place Your Bets" className="w-full mt-5 bg-primaryS rounded-lg border-primaryS ring-0 text-white" onClick={() => alert('Bet placed!')} />
+        
+      </Dialog>
     </div>
   );
 }
