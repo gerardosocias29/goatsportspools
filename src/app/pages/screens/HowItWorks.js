14f4b455cb6f72814cc825aa60962ec9ab8e4d81
Gerardo <g.socias29@gmail.com>
2024-08-25 21:34:02 +0800

    updates

--- src/app/pages/screens/HowItWorks.js
+++ src/app/pages/screens/HowItWorks.js
@@ -5,42 +5,56 @@ const HowItWorks = () => {
         <div className="text-primary text-3xl font-semibold">How It Works</div>
         <div className="w-full p-5 bg-white rounded-lg flex flex-col gap-5">
           <p className="text-lg font-medium">
-            All players helping with our inaugural season will have a chance to win $200. Enter this wager league, and whoever has the highest balance after week 8 will receive $200.
+            All players participating in our inaugural season have a chance to win $200. Enter this wager league for FREE, and the player with the highest balance after Week 8 will receive $200.
           </p>
-          <p className="text-lg font-semibold">Here's how it works:</p>
+          <p className="text-lg font-semibold text-center">Register for free now!</p>
+          
+          <p className="text-lg font-semibold">Here’s how it works:</p>
+          <ol className="list-decimal list-inside space-y-2">
+            <li>Register as a player, and you'll receive an initial balance of $3,000.</li>
+            <li>Place wagers using your balance.</li>
+            <li>Optional: Increase your balance — For $10, you can receive an additional $3,000, bringing your total initial balance to $6,000. This option is available only after your initial registration.</li>
+            <li>Rebuy option — If you lose your entire balance, you can rebuy for $40. After rebuying, your balance will be refilled with $30,000. Unlimited rebuys are allowed until November 16th at 1 p.m.</li>
+          </ol>
+
+          <p className="text-lg">
+            This is a wagering league where all players use their balance to place wagers on weekly NFL games. You can place straight, parlay, or teaser wagers. Spreads are released on Tuesdays before 6 p.m. and are adjusted only once on Fridays at 8 p.m. Central Time. These adjusted spreads will remain unchanged until game time. All spreads are based on Las Vegas oddsmakers.
+          </p>
+
+          <p className="text-lg font-semibold">
+            All buy-ins and rebuys contribute to the prize pool. There are no fees for participating in this wagering league or service.
+          </p>
+          
+          <p className="text-lg">
+            The player with the highest balance on October 29th will win $200. This prize money is provided by GOAT and will not be deducted from the total prize pool.
+          </p>
+          
+          <p className="text-lg">
+            A running total of all buy-ins will be displayed on the home page for players and visitors to see. For example:
+          </p>
           <ul className="list-disc list-inside space-y-2">
-            <li>Register as a player and you will receive 3000 as your initial balance.</li>
-            <li>Use your balance to place wagers.</li>
-            <li>For $10, you can receive another 3000 in your initial balance. Total initial balance will be 6000.</li>
-            <li>Once your whole balance is lost, you can rebuy for $40. After rebuy, your balance will be refilled with 30000.</li>
+            <li>11 initial buy-ins of $10 each, totaling $110</li>
+            <li>8 rebuys of $40 each, totaling $320</li>
           </ul>
+          
           <p className="text-lg">
-            This is a wagering league. All players use their balance to wager in weekly NFL games. You can wager in straight, parlay, or teaser wagers. Spreads are released on Tuesdays before 6pm. These spreads do not adjust during the week until Friday. The spreads will be adjusted one time at 8pm Central time on Fridays. Then these spreads will remain the same until gametime. All spreads will be based on Las Vegas oddsmakers.
+            Total prize pool: $430.
           </p>
+          
           <p className="text-lg font-semibold">
-            All buyins will be awarded in the prize structure. There are no fees in this wagering league or service.
+            GOAT Sportspools will also contribute at least $100 to the final prize pool.
           </p>
+          
           <p className="text-lg">
-            The player with the highest balance on October 29th will win $200.
+            In the scenario above, the highest balance at the end of the wagering season will win $430. The second-highest balance will win $100.
           </p>
-          <p className="text-lg">
-            A running total of all the buyins will be recorded on the home page for all players and visitors to see. For example:
-          </p>
-          <ul className="list-disc list-inside space-y-2">
-            <li>11 initial buyins of $10 totaling $110</li>
-            <li>8 rebuys of $40 totaling $320</li>
-          </ul>
-          <p className="text-lg">
-            Total prize pool is $430.
-          </p>
+          
           <p className="text-lg font-semibold">
-            GoatSportspools will also add to the final prize pool of at least $100.
+            This wagering league will end after the NFL Divisional round, the third week of January 2024.
           </p>
+          
           <p className="text-lg">
-            In the scenario above, the highest balance at the end of the NFL season will win $430. The second-highest balance will win $100.
-          </p>
-          <p className="text-lg font-semibold">
-            Prize structure will be announced after the second week of the NFL season. September 19th, 2024.
+            The prize structure will be announced after the second week of the NFL season, on September 19th, 2024.
           </p>
         </div>
       </div>
