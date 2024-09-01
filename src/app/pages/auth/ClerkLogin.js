25c9e1e39fb0198ac84d60b517cd7aafe33c9c9e
Gerardo <g.socias29@gmail.com>
2024-09-01 10:51:59 +0800

    Update

--- src/app/pages/auth/ClerkLogin.js
+++ src/app/pages/auth/ClerkLogin.js
@@ -35,32 +35,40 @@ export default function ClerkLogin() {
   return (
     // <SignIn />
     <div className="grid md:grid-cols-2 items-center gap-1 min-h-screen">
-      <div className={`rounded-tr-[100px] rounded-br-[100px] relative hidden bg-background bg-cover bg-center bg-no-repeat bg-[url(https://goatsportspools.com/img/newlogo_2.png)] h-full flex-col md:flex md:shrink md:w-full m-auto`}>
-        {/* <div className="absolute w-full h-full bg-black bg-opacity-50 rounded-tr-[100px] rounded-br-[100px]"></div> */}
-        {/* <div className="flex text-[#f7975d] text-3xl lg:text-5xl xl:text-7xl text-center font-sans font-black m-auto z-[1]">Welcome to GoatSportsPools!</div> */}
+      <div className={`rounded-tr-[100px] rounded-br-[100px] relative hidden bg-background bg-cover bg-center bg-no-repeat bg-[url(https://goatsportspools.com/assets/images/logo_expanded.png)] h-full flex-col flex justify-end md:flex md:shrink md:w-full m-auto`}>
+        <div className="absolute w-full h-full bg-black bg-opacity-50 rounded-tr-[100px] rounded-br-[100px]"></div>
+        <div className="text-white text-center flex flex-col z-[1] pb-[50px]">
+          <p className="text-3xl lg:text-5xl xl:text-5xl text-center font-sans font-black">Welcome to GoatSportsPools!</p>
+          <p className="text-md mt-4">FREE to enter - WIN $200</p>
+          <p className="text-md">Initial $10 - 6000 for wagering</p>
+          <p className="text-md">Rebuys will cost $40 for 30000 wagering</p>
+          <p className="hover:underline mt-4 inline-block cursor-pointer text-primaryS" onClick={handleHowItWorks}>
+            CLICK Here to see how it works
+          </p>
+        </div>
         <div className="flex mb-8 text-white z-[1] flex-col p-4 absolute w-full top-0">
-          <div className="bg-white/30 backdrop-blur-sm rounded-lg shadow-lg border border-white/20 p-4 max-w-md">
-            <p className="text-xl font-bold">Welcome to the inaugural Goat Sports Pools!</p>
+          <div className="bg-white/30 backdrop-blur-sm rounded-lg shadow-lg border border-white/20 p-4 max-w-xs">
+            {/* <p className="text-xl font-bold">Welcome to the inaugural Goat Sports Pools!</p>
             <p className="text-md mt-4">FREE to enter - WIN $200</p>
             <p className="text-md">Initial $10 - 6000 for wagering</p>
-            <p className="text-md">Rebuys will cost $40 for 30000 wagering</p>
+            <p className="text-md">Rebuys will cost $40 for 30000 wagering</p> */}
 
-            <p className="text-xs mt-4">1 - Total GOAT Players</p>
+            <p className="text-xs">1 - Total GOAT Players</p>
             <p className="text-xs">1 - Number of $10 GOAT buyins</p>
-            <p className="text-xs">1 - Number rebuys</p>
+            <p className="text-xs">0 - Number rebuys</p>
 
             <p className="text-xs mt-4 font-bold">October Prize pool is $200</p>
-            <p className="text-xs font-bold">End of Season Prize pool is $200</p>
+            <p className="text-xs font-bold">End of Season Prize pool is $100</p>
 
 
-            <p className="hover:underline mt-4 inline-block cursor-pointer text-primaryS" onClick={handleHowItWorks}>
+            {/* <p className="hover:underline mt-4 inline-block cursor-pointer text-primaryS" onClick={handleHowItWorks}>
               CLICK Here to see how it works
-            </p>
+            </p> */}
           </div>
           
         </div>
       </div>
-      <div className="p-5 flex flex-col m-auto w-full h-full bg-cover gap-2 flex items-center justify-center bg-center bg-no-repeat bg-[url('https://goatsportspools.com/img/newlogo_2.png')] md:w-full md:bg-none bg-background md:bg-white">
+      <div className="p-5 flex flex-col m-auto w-full h-full bg-cover gap-2 flex items-center justify-center bg-center bg-no-repeat bg-[url('https://goatsportspools.com/assets/images/logo_expanded.png')] md:w-full md:bg-none bg-background md:bg-white">
         <div className="flex text-white z-[1] flex-col w-full top-0 md:hidden">
           <div className="bg-white/30 backdrop-blur-sm rounded-lg shadow-lg border border-white/20 p-2">
             <p className="text-sm font-bold">Welcome to the inaugural Goat Sports Pools!</p>
