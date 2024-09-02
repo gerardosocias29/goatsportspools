c57f20ecd5f0576b2f36d3adebe252db3b9ff95d
Gerardo <g.socias29@gmail.com>
2024-09-02 21:53:03 +0800

    update

--- src/app/pages/screens/ContactUs.js
+++ src/app/pages/screens/ContactUs.js
@@ -62,7 +62,7 @@ const ContactUs = () => {
       <div className="flex flex-col gap-5 p-5">
         <div className='flex justify-center items-center min-h-screen text-sm p-4 bg-[#fff]'>
           <div className="w-full xl:w-1/3 lg:w-1/2 rounded-[56px] min-h-[500px] p-1" style={{background: 'linear-gradient(180deg, rgb(16 12 68) 100%, rgba(33, 150, 243, 0) 30%)'}}>
-            <div className="rounded-[53px] bg-white border-5 border-transparent w-full h-full lg:p-[5rem] p-[3rem]">
+            <div className="rounded-[53px] bg-white border-5 border-transparent w-full h-full lg:p-[5rem] p-[2rem]">
               <div className="flex flex-col items-center justify-center gap-1 mb-[2rem]">
                 <img src="https://goatsportspools.com/img/favicon.png" width={100} height={100}/>
                 <p className="text-2xl text-center font-bold">Submit any inquiries or questions</p>
@@ -81,8 +81,8 @@ const ContactUs = () => {
                     <label htmlFor="message" className="font-semibold">Your Message</label>
                     <InputTextarea required id="message" value={contactUs.message} placeholder="Your message" onChange={(e) => handleInputChange(e, 'message')} className="text-sm rounded-lg" autoComplete="new-message"/>
                   </div>
-                  <div className="lg:col-span-2">
-                    <Button loading={loading} type="submit" label="Submit" className="rounded-lg ring-0 w-full bg-primary border-none" />
+                  <div className="lg:col-span-2 flex justify-center">
+                    <Button loading={loading} type="submit" label="Submit" className="rounded-lg ring-0 bg-primary border-none" />
                   </div>
                 </div> 
               </form> 
